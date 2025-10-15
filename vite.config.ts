32528fa0f534223aba64f1ee6721bc56c8c0936e
SebPollmanns <136166801+sebpollmanns@users.noreply.github.com>
2025-10-15 16:34:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy':
+        "frame-ancestors https://steuerberatung-pollmanns.de https://www.steuerberatung-pollmanns.de;"
+    }
+  },
+  preview: {
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy':
+        "frame-ancestors https://steuerberatung-pollmanns.de https://www.steuerberatung-pollmanns.de;"
+    }
+  }
 })
